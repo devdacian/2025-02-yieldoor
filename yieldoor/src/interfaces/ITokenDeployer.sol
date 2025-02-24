d402e177dc0aff50c70ecc2c9cac89f9bd06b707
sherlock-admin <95431921+sherlock-admin@users.noreply.github.com>
2025-02-24 10:28:15 +0100

    Delete scope directories

--- yieldoor/src/interfaces/ITokenDeployer.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
-
-interface ITokenDeployer {
-    function deploy(string memory name, string memory symbol, uint8 decimals, address asset, uint256 id)
-        external
-        returns (address);
-}
